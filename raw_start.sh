5a67e8d26399677e71c83ce72c5929eeac7222f8
Noah Duncan <nduncan@predictiveindex.com>
2024-10-18 11:18:48 -0500

    add raw start

---
+++ raw_start.sh
@@ -0,0 +1,4 @@
+pip install nanobind scikit-build-core[pyproject]
+time pip install --no-build-isolation -ve .
+pip install deepgram-sdk
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/python-zoom-linux-sdk/lib/zoomsdk
