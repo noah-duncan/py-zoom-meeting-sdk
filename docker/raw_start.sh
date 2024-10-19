ccd194dbd2c4c953ca128fdd4a6978e1486988f2
Noah Duncan <nduncan@predictiveindex.com>
2024-10-19 12:47:56 -0500

    rearrange slightly

--- docker/raw_start.sh
+++
@@ -1,6 +0,0 @@
-pip install nanobind scikit-build-core[pyproject]
-time pip install --no-build-isolation -ve .
-pip install deepgram-sdk
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/python-zoom-linux-sdk/src/zoomsdk/
-
-
