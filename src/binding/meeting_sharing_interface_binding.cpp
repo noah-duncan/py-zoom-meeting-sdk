862d511d08b125430ed05e9974f643734d4f53ce
Noah Duncan <crusty.noah@gmail.com>
2025-01-27 19:05:28 -0600

    add binding for new struct

--- src/binding/meeting_sharing_interface_binding.cpp
+++ src/binding/meeting_sharing_interface_binding.cpp
@@ -121,4 +121,16 @@ void init_meeting_sharing_interface_binding(nb::module_ &m) {
         .def("onSharedVideoEnded", &IMeetingShareCtrlEvent::onSharedVideoEnded)
         .def("onVideoFileSharePlayError", &IMeetingShareCtrlEvent::onVideoFileSharePlayError)
         .def("onFailedToStartShare", &IMeetingShareCtrlEvent::onFailedToStartShare);
+
+    // Add ZoomSDKSharingSourceInfo struct binding
+    nb::class_<ZoomSDKSharingSourceInfo>(m, "SharingSourceInfo")
+        .def_rw("userid", &ZoomSDKSharingSourceInfo::userid)
+        .def_rw("shareSourceID", &ZoomSDKSharingSourceInfo::shareSourceID)
+        .def_rw("status", &ZoomSDKSharingSourceInfo::status)
+        .def_rw("isShowingInFirstView", &ZoomSDKSharingSourceInfo::isShowingInFirstView)
+        .def_rw("isShowingInSecondView", &ZoomSDKSharingSourceInfo::isShowingInSecondView)
+        .def_rw("isCanBeRemoteControl", &ZoomSDKSharingSourceInfo::isCanBeRemoteControl)
+        .def_rw("contentType", &ZoomSDKSharingSourceInfo::contentType)
+        .def_rw("hwndSharedApp", &ZoomSDKSharingSourceInfo::hwndSharedApp)
+        .def_rw("monitorID", &ZoomSDKSharingSourceInfo::monitorID);
 }
